aefd2578954535de543c838eb276a029064b5da8
Andrew <andrewnguyen17678@gmail.com>
2022-05-19 18:13:39 -0700

    fixed footer "<" tag. and removed redunant script.js scrip tag

--- js/contact-us.js
+++ js/contact-us.js
@@ -7,7 +7,7 @@ window.onload = function() {
     document.getElementById("contact-form").addEventListener("submit", function(event) {
         event.preventDefault();
         // generate a five digit number for the contact_number variable
-        // this.contact_number.value = Math.random() * 100000 | 0;
+        this.contact_number.value = Math.random() * 100000 | 0;
         // these IDs from the previous steps
         emailjs.sendForm('service_bph6ztr', 'template_t6gbcim', this)
             .then(function() {
