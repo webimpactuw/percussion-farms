bb0cbc6656fb51a9d6b0d0f2dee7cdec13713358
blloop <42186044+blloop@users.noreply.github.com>
2023-04-13 19:00:49 -0700

    redefine image sources (home + about)

--- content/load-about.js
+++ content/load-about.js
@@ -5,26 +5,44 @@ var url = 'https://sheets.googleapis.com/v4/spreadsheets/' + SHEET_ID +
 document.addEventListener('DOMContentLoaded', init);
 function init() {
     let start_b = 7;
+    let img_start = 37;
+    let coll_start = 32;
     try {
         fetch(url)
             .then(res => res.text())
             .then(rep => {
-                if (JSON.parse(rep)["values"] != undefined) {
-                    document.getElementById(`t${start_b}`).innerHTML = JSON.parse(rep)["values"][start_b][1];
-                    document.getElementById(`s${start_b}`).innerHTML = JSON.parse(rep)["values"][start_b][2];
-                    document.getElementById(`d${start_b}`).innerHTML = JSON.parse(rep)["values"][start_b][3];
+                let sheet_data = JSON.parse(rep)["values"];
+                if (sheet_data != undefined) {
+                    // Load text
+                    document.getElementById(`t${start_b}`).innerHTML = sheet_data[start_b][1];
+                    document.getElementById(`s${start_b}`).innerHTML = sheet_data[start_b][2];
+                    document.getElementById(`d${start_b}`).innerHTML = sheet_data[start_b][3];
                     for (let i = start_b + 1; i < start_b + 4; i++) {
-                        if (JSON.parse(rep)["values"][i][1] != null) {
-                            document.getElementById(`t${i}`).innerHTML = JSON.parse(rep)["values"][i][1];
-                            document.getElementById(`s${i}`).innerHTML = JSON.parse(rep)["values"][i][2];
-                            document.getElementById(`d${i}`).innerHTML = JSON.parse(rep)["values"][i][3];
+                        if (sheet_data[i][1] != null) {
+                            document.getElementById(`t${i}`).innerHTML = sheet_data[i][1];
+                            document.getElementById(`s${i}`).innerHTML = sheet_data[i][2];
+                            document.getElementById(`d${i}`).innerHTML = sheet_data[i][3];
                             document.getElementById(`mem${i}`).style.height = 'auto';
                         } else {
                             document.getElementById(`mem${i}`).style.visibility = 'hidden';
                         }
                     }
-                    document.getElementById(`t${start_b + 4}`).innerHTML = JSON.parse(rep)["values"][start_b + 4][1];
-                    document.getElementById(`d${start_b + 4}`).innerHTML = JSON.parse(rep)["values"][start_b + 4][3];
+                    document.getElementById(`t${start_b + 4}`).innerHTML = sheet_data[start_b + 4][1];
+                    document.getElementById(`d${start_b + 4}`).innerHTML = sheet_data[start_b + 4][3];
+
+                    // Load images
+                    for (let i = img_start; i < img_start + 3; i++) {
+                        if (sheet_data[i][3]) {
+                            document.getElementById(`img-${i - img_start + 11}`).src = sheet_data[i][3];
+                            document.getElementById(`img-${i - img_start + 11}`).alt = sheet_data[i][1];
+                        }
+                    }
+                    document.getElementById(`img-14`).src = sheet_data[img_start + 3][3];
+                    document.getElementById(`img-14`).alt = sheet_data[img_start + 3][1];
+                    for (let i = coll_start; i < coll_start + 4; i++) {
+                        document.getElementById(`img-${i - coll_start + 6}`).src = sheet_data[i][3];
+                        document.getElementById(`img-${i - coll_start + 6}`).alt = sheet_data[i][1];
+                    }
                 }
             })
     } catch (err) {
--- content/load-home.js
+++ content/load-home.js
@@ -2,21 +2,33 @@ const base = `https://docs.google.com/spreadsheets/d/${SHEET_ID}`;
 const query = encodeURIComponent("Select *");
 var url = 'https://sheets.googleapis.com/v4/spreadsheets/' + SHEET_ID + 
     '/values/' + SHEET_NAME + '?alt=json&key=' + API_KEY;
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', init, ['blocking', 'responseHeaders', 'extraHeaders']);
 
 function init() {
     let start_a = 1;
     let length_a = 6;
+    let img_start = 27;
     try {
         fetch(url)
             .then(res => res.text())
             .then(rep => {
-                if (JSON.parse(rep)["values"] != undefined) {
+                let sheet_data = JSON.parse(rep)["values"];
+                if (sheet_data != undefined) {
+                    // Load text
                     for (let i = start_a; i < start_a + length_a; i++) {
-                        document.getElementById(`t${i}`).innerHTML = JSON.parse(rep)["values"][i][1];
-                        document.getElementById(`d${i}`).innerHTML = JSON.parse(rep)["values"][i][3];
+                        document.getElementById(`t${i}`).innerHTML = sheet_data[i][1];
+                        document.getElementById(`d${i}`).innerHTML = sheet_data[i][3];
                     }
-                    document.getElementById('s18').href = JSON.parse(rep)["values"][18][2];
+                    document.getElementById('s18').href = sheet_data[18][2];
+
+                    // Load images
+                    for (let i = img_start; i < img_start + 9; i++) {
+                        if (sheet_data[i][3]) {
+                            document.getElementById(`img-${i - img_start + 1}`).alt = sheet_data[i][1];
+                            document.getElementById(`img-${i - img_start + 1}`).src = sheet_data[i][3];
+                        }
+                    }
+                    // SOON: Check if fresh sheet has an image
                 }                        
             })
     } catch (err) {
