2b606678f9a89c098d631be64c5294a4877b72a7
blloop <42186044+blloop@users.noreply.github.com>
2023-07-29 22:27:47 -0700

    add env import

--- content/env.js
+++ content/env.js
@@ -1,3 +1,8 @@
 const API_KEY = 'AIzaSyCO-swZtwFWvAjlascpWEr1ziiQBtEbImY';
 const SHEET_ID = '1PwUAtyk04btuc6Ohaf68CYxcFmHtLBhAaMVvQ96GWSI';
-const SHEET_NAME = 'Sheet1'
+const SHEET_NAME = 'Sheet1';
+
+
+console.log(MY_SECRET);
+console.log(`$MY_SECRET`);
+console.log($MY_SECRET);
